886bd69b62570c8600897a5d4467ee0efcc4b7e3
Gbadamson <gbadamosi.abubakar.95@gmail.com>
2021-08-18 04:46:07 +0100

    just a little stuff to do

---
+++ myname.js
@@ -0,0 +1,7 @@
+//this simple program is going to print out my name in the console
+
+const firstName = "Abubakar";
+const lastName = "Gbadamosi";
+
+const fullName = lastName + " " + firstName;
+console.log(fullName);
